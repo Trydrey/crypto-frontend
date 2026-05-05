11d7d23efe318d05beee93c3fb8518c9a3df4ed7
dreyaqp <dreyqp@gmail.com>
2026-05-05 20:36:56 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
@@ -11,49 +11,54 @@ import Dashboard from './pages/Dashboard';
 import AssetDetail from './pages/AssetDetail';
 import Transactions from './pages/Transactions';
 import WarningBanner from './components/WarningBanner';
-// import './index.css'; // Comment this out if using CDN
+
+// 🔒 ProtectedRoute: redirects to /signin if user is not logged in
+function ProtectedRoute({ children }) {
+  const isLoggedIn = localStorage.getItem('user'); // Update this to match your auth logic
+  return isLoggedIn ? children : <Navigate to="/signin" replace />;
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-pink-50 flex flex-col"> {/* Changed from bg-gray-50 to bg-pink-50 */}
+      <div className="min-h-screen bg-pink-50 flex flex-col">
         <Navbar />
         <div className="flex-grow">
           <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/explore" element={<Explore />} />
-  <Route path="/learn" element={<Learn />} />
-  <Route path="/signin" element={<SignIn />} />
-  <Route path="/signup" element={<SignUp />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/learn" element={<Learn />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
 
-  {/* 🔒 Protected routes */}
-  <Route
-    path="/dashboard"
-    element={
-      <ProtectedRoute>
-        <Dashboard />
-      </ProtectedRoute>
-    }
-  />
+            {/* 🔒 Protected routes */}
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
 
-  <Route
-    path="/transactions"
-    element={
-      <ProtectedRoute>
-        <Transactions />
-      </ProtectedRoute>
-    }
-  />
+            <Route
+              path="/transactions"
+              element={
+                <ProtectedRoute>
+                  <Transactions />
+                </ProtectedRoute>
+              }
+            />
 
-  <Route
-    path="/asset/:symbol"
-    element={
-      <ProtectedRoute>
-        <AssetDetail />
-      </ProtectedRoute>
-    }
-  />
-</Routes>
+            <Route
+              path="/asset/:symbol"
+              element={
+                <ProtectedRoute>
+                  <AssetDetail />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
         </div>
         <Footer />
       </div>
