2c8be1ac47f5093e2fd397b24d9865b9ff50ecc2
Trydrey <dreyaqp@gmail.com>
2026-05-05 00:28:24 +0000

    Update SignIn.jsx

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '../components/common/Button';
 import { login } from '../api/auth';
 import { useNavigate } from 'react-router-dom';
