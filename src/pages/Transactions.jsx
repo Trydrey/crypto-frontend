a9b4903d1380c809198795de65f96e305e6718d6
Trydrey <dreyaqp@gmail.com>
2026-05-05 00:27:32 +0000

    Update Transactions.jsx

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '../components/common/Button';
 import { useNavigate } from 'react-router-dom';
 
